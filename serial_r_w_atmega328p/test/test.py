6052b990fe7cd149876043abd1dbd767038498e2
mllukasik <tvpozytyw@gmail.com>
2025-02-18 22:22:05 +0100

    serial r/w for atmega238p update test script

--- serial_r_w_atmega328p/test/test.py
+++ serial_r_w_atmega328p/test/test.py
@@ -1,5 +1,19 @@
 import serial
-ser = serial.Serial('/dev/ttyACM0')
-ser.write(b'1')
-print(ser.read())
-ser.close()
+
+def get_serial():
+    ser = serial.Serial()
+    ser.port = '/dev/ttyACM0'
+    ser.baudrate = 9600
+    ser.bytesize =  serial.EIGHTBITS
+    ser.parity = serial.PARITY_NONE
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.open()
+    return ser
+
+if __name__ == "__main__":
+    ser = get_serial()
+    print("write")
+    ser.write(b'a')
+    print("read")
+    print(ser.read())
+    ser.close()
